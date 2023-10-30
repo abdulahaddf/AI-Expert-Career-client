ec637cf63e303ded3d555aca6e8e3077ed2ca0da
abdulahaddf <abdulahad.df1@gmail.com>
2023-10-30 23:52:51 +0600

    consultant booking form updated

--- src/components/pages/AiConsultantProfile/Expertise.jsx
+++ src/components/pages/AiConsultantProfile/Expertise.jsx
@@ -24,12 +24,14 @@ const Expertise = ({ consultant }) => {
     workingWith,
     selectedDays,
   } = consultant;
-  const [userinfo] = UseUser();
+  // const [userinfo] = UseUser();
   const { language } = useContext(MyContext);
   const [hideButton, setHideButton] = useState(false);
-  const { control, handleSubmit, reset } = useForm({
-    defaultValues: { email: userinfo?.email },
-  });
+  // const { control, handleSubmit, reset } = useForm({
+  //   defaultValues: { email: userinfo?.email },
+  //   problemType: "Career Consulting", 
+  // });
+  const { handleSubmit, register, reset } = useForm();
 
   // console.log(conMail);
 
@@ -85,8 +87,8 @@ const Expertise = ({ consultant }) => {
       const responseData = await apiResponse.json();
 
       if (responseData.insertedId) {
-        toast.success("You've Successfully requested for the appointment");
         reset();
+        toast.success("You've Successfully requested for the appointment");
       }
     } catch (error) {
       console.error("Error:", error);
@@ -126,179 +128,115 @@ const Expertise = ({ consultant }) => {
 
       {hideButton && (
         <div className="mt-5 bg-[#FFFFFF]/30">
-          <form onSubmit={handleSubmit(onSubmit)} className="space-y-5 section">
-            <div>
-              <label htmlFor="name" className="text-xl font-semibold">
-                Name*
-              </label>
-              <Controller
-                name="name"
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <input
-                    {...field}
-                    type="text"
-                    placeholder="Enter your name"
-                    className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
-                  />
-                )}
-              />
-            </div>
-            <div>
-              <label htmlFor="email" className="text-xl font-semibold">
-                Email
-              </label>
-              <Controller
-                name="email"
-                control={control}
-                // rules={{ required: true }}
-                render={({ field }) => (
-                  <input
-                    {...field}
-                    value={userinfo.email}
-                    type="email"
-                    placeholder="Enter your email address"
-                    className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
-                  />
-                )}
-              />
-            </div>
-            <div>
-              <label htmlFor="phone" className="text-xl font-semibold">
-                Phone number
-              </label>
-              <Controller
-                name="phone"
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <input
-                    {...field}
-                    type="tel"
-                    placeholder="Enter your phone number"
-                    className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
-                  />
-                )}
-              />
-            </div>
-            <div>
-              <label htmlFor="appointDate" className="text-xl font-semibold">
-                Appointment date
-              </label>
-              <Controller
-                name="appointDate"
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <input
-                    {...field}
-                    type="date"
-                    placeholder="Select an appointment date"
-                    className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
-                  />
-                )}
-              />
-            </div>
-            <div>
-              <label htmlFor="appointTime" className="text-xl font-semibold">
-                Appointment time
-              </label>
-              <Controller
-                name="appointTime"
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <input
-                    {...field}
-                    type="time"
-                    placeholder="Select an appointment time"
-                    className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
-                  />
-                )}
-              />
-            </div>
-            <div>
-              <label htmlFor="problemType" className="text-xl font-semibold">
-                Type of Problem
-              </label>
-              <Controller
-                name="problemType"
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <select
-                    {...field}
-                    className="py-3 appearance-none px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
-                    defaultValue="Career Consulting"
-                  >
-                    <option value="Career Consulting">Career Consulting</option>
-                    <option value="Project">Project</option>
-                    <option value="Research">Research</option>
-                  </select>
-                )}
-              />
-            </div>
-            <div>
-              <label htmlFor="fileLink" className="text-xl font-semibold">
-                Insert a file link (optional)
-              </label>
-              <Controller
-                name="fileLink"
-                control={control}
-                render={({ field }) => (
-                  <input
-                    {...field}
-                    type="url"
-                    placeholder="Enter a file link (optional)"
-                    className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
-                  />
-                )}
-              />
-            </div>
-            <div>
-              <label htmlFor="caseSummary" className="text-xl font-semibold">
-                Case summary (optional)
-              </label>
-              <Controller
-                name="caseSummary"
-                control={control}
-                render={({ field }) => (
-                  <textarea
-                    {...field}
-                    placeholder="Enter case summary (optional)"
-                    rows="4"
-                    className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
-                  ></textarea>
-                )}
-              />
-            </div>
-            <div className="flex items-center">
-              <label htmlFor="urgent" className="text-xl text-primary mr-2">
-                Is it urgent?
-              </label>
-              <Controller
-                name="urgent"
-                control={control}
-                render={({ field }) => (
-                  <input
-                    {...field}
-                    type="checkbox"
-                    className="checkbox checkbox-xs checkbox-error"
-                  />
-                )}
-              />
-            </div>
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-5 section">
+        <div>
+          <label htmlFor="name" className="text-xl font-semibold">
+            Name*
+          </label>
+          <input
+            {...register("name", { required: true })}
+            type="text"
+            placeholder="Enter your name"
+            className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
+          />
+        </div>
+        <div>
+          <label htmlFor="email" className="text-xl font-semibold">
+            Email
+          </label>
+          <input
+            {...register("email")}
+            type="email"
+            placeholder="Enter your email address"
+            className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
+          />
+        </div>
+        <div>
+          <label htmlFor="phone" className="text-xl font-semibold">
+            Phone number
+          </label>
+          <input
+            {...register("phone", { required: true })}
+            type="tel"
+            placeholder="Enter your phone number"
+            className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
+          />
+        </div>
+        <div>
+          <label htmlFor="appointDate" className="text-xl font-semibold">
+            Appointment date
+          </label>
+          <input
+            {...register("appointDate", { required: true })}
+            type="date"
+            placeholder="Select an appointment date"
+            className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
+          />
+        </div>
+        <div>
+          <label htmlFor="appointTime" className="text-xl font-semibold">
+            Appointment time
+          </label>
+          <input
+            {...register("appointTime", { required: true })}
+            type="time"
+            placeholder="Select an appointment time"
+            className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
+          />
+        </div>
+        <div>
+          <label htmlFor="problemType" className="text-xl font-semibold">
+            Type of Problem
+          </label>
+          <select
+            {...register("problemType", { required: true })}
+            className="py-3 appearance-none px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
 
-            <div className="flex justify-center">
-              <button
-                type="submit"
-                className="py-[12px] px-[78px] text-xl font-bold bg-[#F6002C] text-white rounded-2xl mt-5"
-              >
-                {language === "bn" ? "জমা দিন" : "Submit"}
-              </button>
-            </div>
-          </form>
+          >
+            <option value="Career Consulting">Career Consulting</option>
+            <option value="Project">Project</option>
+            <option value="Research">Research</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="fileLink" className="text-xl font-semibold">
+            Insert a file link (optional)
+          </label>
+          <input
+            {...register("fileLink")}
+            type="url"
+            placeholder="Enter a file link (optional)"
+            className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
+          />
+        </div>
+        <div>
+          <label htmlFor="caseSummary" className="text-xl font-semibold">
+            Case summary (optional)
+          </label>
+          <textarea
+            {...register("caseSummary")}
+            placeholder="Enter case summary (optional)"
+            rows="4"
+            className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
+          ></textarea>
+        </div>
+        <div className="flex items-center">
+          <label htmlFor="urgent" className="text-xl text-primary mr-2">
+            Is it urgent?
+          </label>
+          <input
+            {...register("urgent")}
+            type="checkbox"
+            className="checkbox checkbox-xs checkbox-error"
+          />
+        </div>
+
+        <div className="flex justify-center">
+          <button type="submit" className="btn-view">
+            Submit
+          </button>
+        </div>
+      </form>
         </div>
       )}
     </div>
