4da17335de6839a674ddbfbcf6bb927bce509015
abdulahaddf <abdulahad.df1@gmail.com>
2023-11-03 10:00:29 +0600

    updated

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -15,7 +15,7 @@ import Roadmap from "./Sections/Roadmap";
 import HomeConsultantSection from "./Sections/HomeConsultantsection";
 import { Link } from "react-router-dom";
 import { AiFillUpCircle } from "react-icons/ai";
-import { GoMoveToTop} from "react-icons/go";
+import { GoMoveToTop } from "react-icons/go";
 import { FaArrowCircleUp } from "react-icons/fa";
 
 const Home = () => {
@@ -38,50 +38,50 @@ const Home = () => {
       window.removeEventListener("scroll", scrollEffect);
     };
   }, []);
-   // scrollTo
-   useEffect(() => {
+  // scrollTo
+  useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }, []);
   return (
     <>
-     
       <HeroAria />
       <div className=" px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl lg:px-8">
         {/* <HomeSearch /> */}
         <HomeAbout />
         <HomeCourseSection />
-        <Roadmap/>
-        <HomeConsultantSection/>
+        <Roadmap />
+        <HomeConsultantSection />
         {/* <HomeHeading /> */}
-      <HomeBlogsSection/>
+        <HomeBlogsSection />
         <HomeTestimonial />
         {/* <HomeEvent /> */}
         <HomePartnership />
       </div>
 
       <div
-  className={`transform ${scrollNav ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-1000 fixed right-1 bottom-2 z-50  rounded-full`}
->
-  <div
-    className={` md:tooltip tooltip-left  duration-200 ${scrollNav ? 'opacity-100' : 'opacity-0'} transition-opacity duration-1000`}
-    data-tip="scroll to top"
-  >
-    {scrollNav && (
-      <Link
-        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-        spy={true}
-        smooth={true}
-        offset={-150}
-        duration={500}
+        className={`transform ${
+          scrollNav ? "translate-x-0" : "translate-x-full"
+        } transition-transform duration-1000 fixed right-1 bottom-2 z-50  rounded-full`}
       >
-        <FaArrowCircleUp className=" font-semibold rounded-full shadow-xl text-2xl md:text-2xl md:w-8 md:h-8 text-black"/>
-      </Link>
-    )}
-  </div>
-</div>
-
-  
- 
+        <div
+          className={` md:tooltip tooltip-left  duration-200 ${
+            scrollNav ? "opacity-100" : "opacity-0"
+          } transition-opacity duration-1000`}
+          data-tip="scroll to top"
+        >
+          {scrollNav && (
+            <Link
+              onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+              spy={true}
+              smooth={true}
+              offset={-150}
+              duration={500}
+            >
+              <FaArrowCircleUp className=" font-semibold rounded-full shadow-xl text-2xl md:text-2xl md:w-8 md:h-8 text-black" />
+            </Link>
+          )}
+        </div>
+      </div>
     </>
   );
 };
