fd300fe171ea10f230ea440ac9c06b5cd19b244f
abdulahaddf <abdulahad.df1@gmail.com>
2023-11-05 00:00:07 +0600

    consultant page updated for consultant

--- src/components/pages/AiConsultantProfile/Expertise.jsx
+++ src/components/pages/AiConsultantProfile/Expertise.jsx
@@ -6,6 +6,7 @@ import { toast } from "react-toastify";
 import UseUser from "../../../hooks/useUser";
 import { SiSocketdotio } from "react-icons/si";
 import { FaRegDotCircle } from "react-icons/fa";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Expertise = ({ consultant }) => {
   const {
@@ -89,6 +90,7 @@ const Expertise = ({ consultant }) => {
       if (responseData.insertedId) {
         reset();
         toast.success("You've Successfully requested for the appointment");
+        document.getElementById('my_modal_5').close()
       }
     } catch (error) {
       console.error("Error:", error);
@@ -97,7 +99,7 @@ const Expertise = ({ consultant }) => {
   };
 
   return (
-    <div>
+    <div >
       <h3 className="font-semibold text-2xl">Availability</h3>
       <div className=" space-y-2 section text-lg ml-2 mt-2">
         {selectedDays?.map((d) => (
@@ -117,14 +119,158 @@ const Expertise = ({ consultant }) => {
         ))}
       </div>
 
-      <div className="flex justify-center mt-12">
+      <div className="flex justify-center md:mt-12 ">
         <button
           onClick={() => setHideButton(true)}
-          className=" py-2 px-5 bg-[#ED1B24] rounded-[5px] text-white font-semibold"
+          className=" py-2 px-5 bg-[#ED1B24] rounded-[5px] text-white font-semibold hidden md:block"
         >
           {language == "bn" ? "এপয়েন্টমেন্ট বুক করুন" : "Book Your Appointment"}
         </button>
       </div>
+
+
+{/* Fixed button for mobile */}
+      <div className="bg-white rounded-lg w-full fixed bottom-0 p-2   z-10 md:hidden pr-8"> 
+  <button
+  onClick={()=>document.getElementById('my_modal_5').showModal()}
+  className=" btn-view-red w-full ">{language == "bn" ? "এপয়েন্টমেন্ট বুক করুন" : "Book Your Appointment"} <IoIosArrowForward/></button>
+</div>
+
+
+
+{/* <button className="btn" onClick={()=>document.getElementById('my_modal_5').showModal()}>open modal</button> */}
+<dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+  <div className="modal-box">
+  <form method="dialog">
+      {/* if there is a button in form, it will close the modal */}
+      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 bg-primary text-white hover:bg-primary">✕</button>
+    </form>
+  <form onSubmit={handleSubmit(onSubmit)} className="space-y-5 section">
+        <div>
+          <label htmlFor="name" className="text-xl font-semibold">
+            Name*
+          </label>
+          <input
+            {...register("name", { required: true })}
+            type="text"
+            placeholder="Enter your name"
+            className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
+          />
+        </div>
+        <div>
+          <label htmlFor="email" className="text-xl font-semibold">
+            Email*
+          </label>
+          <input
+            {...register("email", { required: true })}
+            type="email"
+            placeholder="Enter your email address"
+            className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
+          />
+        </div>
+        <div>
+          <label htmlFor="phone" className="text-xl font-semibold">
+            Phone number*
+          </label>
+          <input
+            {...register("phone", { required: true })}
+            type="tel"
+            placeholder="Enter your phone number"
+            className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
+          />
+        </div>
+        <div>
+          <label htmlFor="appointDate" className="text-xl font-semibold">
+            Appointment date*
+          </label>
+          <input
+            {...register("appointDate", { required: true })}
+            type="date"
+            placeholder="Select an appointment date"
+            className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
+          />
+        </div>
+        <div>
+          <label htmlFor="appointTime" className="text-xl font-semibold">
+            Appointment time*
+          </label>
+          <input
+            {...register("appointTime", { required: true })}
+            type="time"
+            placeholder="Select an appointment time"
+            className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
+          />
+        </div>
+        <div>
+          <label htmlFor="problemType" className="text-xl font-semibold">
+            Type of Problem*
+          </label>
+          <select
+            {...register("problemType", { required: true })}
+            className="py-3 appearance-none px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
+
+          >
+            <option value="Career Consulting">Career Consulting</option>
+            <option value="Project">Project</option>
+            <option value="Research">Research</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="fileLink" className="text-xl font-semibold">
+            Insert a file link (optional)
+          </label>
+          <input
+            {...register("fileLink")}
+            type="url"
+            placeholder="Enter a file link (optional)"
+            className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
+          />
+        </div>
+        <div>
+          <label htmlFor="caseSummary" className="text-xl font-semibold">
+            Case summary (optional)
+          </label>
+          <textarea
+            {...register("caseSummary")}
+            placeholder="Enter case summary (optional)"
+            rows="4"
+            className="py-2 px-4 block w-full mt-4 outline-none border border-[#ED1B24]/80"
+          ></textarea>
+        </div>
+        <div className="flex items-center">
+          <label htmlFor="urgent" className="text-xl text-primary mr-2">
+            Is it urgent?
+          </label>
+          <input
+            {...register("urgent")}
+            type="checkbox"
+            className="checkbox checkbox-xs checkbox-error"
+          />
+        </div>
+
+        <div className="flex justify-center">
+          <button type="submit" className="btn-view">
+            Submit
+          </button>
+        </div>
+      </form>
+    <div className="modal-action">
+     
+    </div>
+  </div>
+</dialog>
+
+
+
+
+
+
+
+
+
+
+
+
 
       {hideButton && (
         <div className="mt-5 bg-[#FFFFFF]/30">
