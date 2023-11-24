69916265a545462985e3c04672536bbdbd5f171e
abdulahaddf <abdulahad.df1@gmail.com>
2023-11-25 01:55:45 +0600

    error page updated

--- src/components/error/Error.jsx
+++ src/components/error/Error.jsx
@@ -4,14 +4,15 @@ import animationData from "../../assets/animation/error.json";
 
 const Error = () => {
   return (
-    <section className="relative flex items-center h-screen p-16 bg-gray-100 text-gray-900">
-      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+   <div>
+    <section className="relative flex  items-center justify-center h-screen md:p-16 pr-5  bg-gray-100 text-gray-900">
+      <div className="container flex flex-col items-center justify-center md:px-5 mx-auto md:my-8">
         <Lottie
           className="select-none pointer-events-none no-select md:w-1/3"
           animationData={animationData}
           loop={true}
         />
-        <p className="text-xl">Something Went Wrong</p>
+         <p className="text-3xl md:text-5xl ">Something Went Wrong</p>
         <Link
           to="/"
           className=" px-8 py-3 font-semibold rounded-full bg-black text-white md:text-md my-4 "
@@ -20,6 +21,7 @@ const Error = () => {
         </Link>
       </div>
     </section>
+   </div>
   );
 };
 
